4dd76a3b2b2d6243a32a8b2b340e10079d7b3969
Summer Rizzo <srainrizzo@gmail.com>
2019-12-05 20:59:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,6 +6,13 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  row_index = 0
+  string_arr = []
+  while row_index < src.count do
+    string_arr.push("I love #{src[row_index][0]} and #{src[row_index][1] on my pizza}")
+    row_index += 1
+  end
+  string_arr
 end
 
 def find_greater_pair(src)
